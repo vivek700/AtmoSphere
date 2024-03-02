3047cc3200b03bede69bf576b348a9ca59e04054
Vivek <vs690799@gmail.com>
2024-03-02 14:42:51 +0530

    bug fix

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,16 +1,17 @@
 'use server'
 
 export const FetchData = async (latitude: number, longitude: number) => {
+    const apiKey: string = process.env.API_KEY || ""
 
     // api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={API key} forcast link
  
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=48c3d9ab0208503e8a1633af30575ebb`, { cache: 'no-store' })
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`, { cache: 'no-store' })
 
     if (!res.ok) {
         throw new Error('Failed to fetch')
     }
     const data = await res.json()
-    // console.log(data)
+    console.log(data)
     return data
 }
 
