b338926a2dd3d307052dd115f824b767a0d4bc0d
Vivek <vs690799@gmail.com>
2024-03-18 20:59:24 +0530

    minor ui fix

--- app/weather/layout.tsx
+++ app/weather/layout.tsx
@@ -7,7 +7,7 @@ import { Metadata } from "next";
 const Layout = ({ children }: { children: React.ReactNode }) => {
 
     return (
-        <div className="max-w-6xl w-11/12 my-5 mx-auto">
+        <div className="max-w-6xl w-full md:w-11/12 md:my-8  mx-auto">
             <BackgroundGradient className="overflow-hidden rounded-[22px]  p-4 sm:p-10 bg-zinc-900">{children}</BackgroundGradient>
         </div >
     )
